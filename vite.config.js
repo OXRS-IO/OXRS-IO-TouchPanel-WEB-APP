237fb43215c087ff418d43020efc27b1f3ebd774
Aaron Knox <aaron_knox@me.com>
2023-04-06 16:40:25 +0100

    updated gh pages settings

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-  base: "/OXRS-IO.github.io/",  
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
