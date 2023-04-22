8d774de88d46a422cdd9091f427335ac5ba68b03
Aaron Knox <aaron_knox@me.com>
2023-04-22 23:59:18 +0100

    Adding Guthub Actions for Auto Deployments

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import router from './router'
 
 const app = createApp(App)
 
-app.config.globalProperties.$version = "2023-04-22T22:40:18.429Z"
+app.config.globalProperties.$version = "2023-04-22T22:47:22.567Z"
 app.use(router)
 app.mount('#app')
