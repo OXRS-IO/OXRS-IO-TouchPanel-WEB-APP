6aafb11a4056f7096a1d05cd8ba0e6706f95a5f1
Aaron Knox <aaron_knox@me.com>
2023-04-22 23:39:36 +0100

    fix CNAME overide

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import router from './router'
 
 const app = createApp(App)
 
-app.config.globalProperties.$version = "2023-04-22T21:49:34.349Z"
+app.config.globalProperties.$version = "2023-04-22T22:33:04.177Z"
 app.use(router)
 app.mount('#app')
